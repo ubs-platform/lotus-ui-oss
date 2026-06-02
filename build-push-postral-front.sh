d3f3df3fa0843c970f4487907612502ffe85c8b4
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-06-02 23:13:36 +0300

    dev

--- build-push-postral-front.sh
+++ build-push-postral-front.sh
@@ -1,5 +1,5 @@
 export IMAGE_NAME='hcangunduz/tk-postral-core-frontend'
-export IMAGE_TAG='latest'
+export IMAGE_TAG='1.0.2-beta'
 export DOCKER_FILE="./dockerfile-postral-core-front"
 docker build --file ${DOCKER_FILE} -t ${IMAGE_NAME}:${IMAGE_TAG} .
 docker push ${IMAGE_NAME}:${IMAGE_TAG}
